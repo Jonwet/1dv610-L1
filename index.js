04ff4038a2615524c16031fdff13930a7d9f9599
Jonatan <jw223tb@student.lnu.se>
2025-09-03 14:33:41 +0200

    Add readline

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello " + process.argv[2] + "!")
+import {createInterface} from 'node:readline/promises'
+
+const readline = createInterface({input: process.stdin, output: process.stdout})
+
+const name = await readline.question('What is your name? ')
+
+console.log(`Hello, ${name}!`)
+
+readline.close()
